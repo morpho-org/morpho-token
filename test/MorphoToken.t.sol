335406f2300bdc1b7d847364fb7f4809a960dc1d
MerlinEgalite <egalite.merlin@gmail.com>
2024-10-10 10:27:59 +0200

    test: add tests to do

--- test/MorphoToken.t.sol
+++ test/MorphoToken.t.sol
@@ -4,7 +4,14 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {MorphoToken} from "../src/MorphoToken.sol";
 
-contract CounterTest is Test {
+// TODO: Test the following:
+// - Test every paths
+// - Test migration flow
+// - Test bundler wrapping
+// - Test access control
+// - Test voting
+// - Test delegation
+contract MorphoTokenTest is Test {
     MorphoToken public token;
 
     function setUp() public {}

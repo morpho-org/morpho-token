7420d9a6c5a6350fc612993d47f2c107cfda7f75
MerlinEgalite <egalite.merlin@gmail.com>
2024-10-16 10:44:20 +0200

    refactor: clean contract

--- src/ERC20DelegatesUpgradeable.sol
+++ src/ERC20DelegatesUpgradeable.sol
@@ -11,16 +11,14 @@ import {EIP712Upgradeable} from
     "lib/openzeppelin-contracts-upgradeable/contracts/utils/cryptography/EIP712Upgradeable.sol";
 import {Initializable} from "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 
-/**
- * @dev Extension of ERC20 to support token delegation.                  |
- *
- * This extension keeps track of each account's vote power. Vote power can be delegated eithe by calling the
- * {delegate} function directly, or by providing a signature to be used with {delegateBySig}. Voting power can be
- * queried through the public accessor {getVotes}.
- *
- * By default, token balance does not account for voting power. This makes transfers cheaper. The downside is that it
- * requires users to delegate to themselves in order to activate their voting power.
- */
+/// @dev Extension of ERC20 to support token delegation.                  |
+///
+/// This extension keeps track of each account's vote power. Vote power can be delegated either by calling the
+/// {delegate} function directly, or by providing a signature to be used with {delegateBySig}. Voting power can be
+/// queried through the public accessor {getVotes}.
+///
+/// By default, token balance does not account for voting power. This makes transfers cheaper. The downside is that it
+/// requires users to delegate to themselves in order to activate their voting power.
 abstract contract ERC20DelegatesUpgradeable is
     Initializable,
     ERC20Upgradeable,
@@ -28,19 +26,25 @@ abstract contract ERC20DelegatesUpgradeable is
     NoncesUpgradeable,
     IDelegates
 {
+    /* CONSTANTS */
+
     bytes32 private constant DELEGATION_TYPEHASH =
         keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
 
-    /// @custom:storage-location erc7201:morpho.storage.Delegates
+    // keccak256(abi.encode(uint256(keccak256("morpho.storage.ERC20Delegates")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant ERC20DelegatesStorageLocation =
+        0x1dc92b2c6e971ab6e08dfd7dcec0e9496d223ced663ba2a06543451548549500;
+
+    /* STRUCTS */
+
+    /// @custom:storage-location erc7201:morpho.storage.ERC20Delegates
     struct ERC20DelegatesStorage {
         mapping(address account => address) _delegatee;
         mapping(address delegatee => uint256) _votingPower;
         uint256 _totalVotingPower;
     }
 
-    // keccak256(abi.encode(uint256(keccak256("morpho.storage.ERC20Delegates")) - 1)) & ~bytes32(uint256(0xff))
-    bytes32 private constant ERC20DelegatesStorageLocation =
-        0x1dc92b2c6e971ab6e08dfd7dcec0e9496d223ced663ba2a06543451548549500;
+    /* FUNCTIONS */
 
     function _getDelegatesStorage() private pure returns (ERC20DelegatesStorage storage $) {
         assembly {
@@ -48,41 +52,31 @@ abstract contract ERC20DelegatesUpgradeable is
         }
     }
 
-    /**
-     * @dev Returns the current amount of votes that `account` has.
-     */
+    /// @dev Returns the current amount of votes that `account` has.
     function getVotes(address account) public view virtual returns (uint256) {
         ERC20DelegatesStorage storage $ = _getDelegatesStorage();
         return $._votingPower[account];
     }
 
-    /**
-     * @dev Returns the current total supply of votes.
-     */
+    /// @dev Returns the current total supply of votes.
     function _getTotalSupply() internal view virtual returns (uint256) {
         ERC20DelegatesStorage storage $ = _getDelegatesStorage();
         return $._totalVotingPower;
     }
 
-    /**
-     * @dev Returns the delegate that `account` has chosen.
-     */
+    /// @dev Returns the delegate that `account` has chosen.
     function delegates(address account) public view virtual returns (address) {
         ERC20DelegatesStorage storage $ = _getDelegatesStorage();
         return $._delegatee[account];
     }
 
-    /**
-     * @dev Delegates votes from the sender to `delegatee`.
-     */
+    /// @dev Delegates votes from the sender to `delegatee`.
     function delegate(address delegatee) public virtual {
         address account = _msgSender();
         _delegate(account, delegatee);
     }
 
-    /**
-     * @dev Delegates votes from signer to `delegatee`.
-     */
+    /// @dev Delegates votes from signer to `delegatee`.
     function delegateBySig(address delegatee, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s)
         public
         virtual
@@ -97,11 +91,9 @@ abstract contract ERC20DelegatesUpgradeable is
         _delegate(signer, delegatee);
     }
 
-    /**
-     * @dev Delegate all of `account`'s voting units to `delegatee`.
-     *
-     * Emits events {IVotes-DelegateChanged} and {IVotes-DelegateVotesChanged}.
-     */
+    /// @dev Delegate all of `account`'s voting units to `delegatee`.
+    ///
+    /// Emits events {IDelegates-DelegateChanged} and {IDelegates-DelegateVotesChanged}.
     function _delegate(address account, address delegatee) internal virtual {
         ERC20DelegatesStorage storage $ = _getDelegatesStorage();
         address oldDelegate = delegates(account);
@@ -111,10 +103,8 @@ abstract contract ERC20DelegatesUpgradeable is
         _moveDelegateVotes(oldDelegate, delegatee, _getVotingUnits(account));
     }
 
-    /**
-     * @dev Transfers, mints, or burns voting units. To register a mint, `from` should be zero. To register a burn, `to`
-     * should be zero. Total supply of voting units will be adjusted with mints and burns.
-     */
+    /// @dev Transfers, mints, or burns voting units. To register a mint, `from` should be zero. To register a burn, `to`
+    /// should be zero. Total supply of voting units will be adjusted with mints and burns.
     function _transferVotingUnits(address from, address to, uint256 amount) internal virtual {
         ERC20DelegatesStorage storage $ = _getDelegatesStorage();
         if (from == address(0)) {
@@ -126,9 +116,7 @@ abstract contract ERC20DelegatesUpgradeable is
         _moveDelegateVotes(delegates(from), delegates(to), amount);
     }
 
-    /**
-     * @dev Moves delegated votes from one delegate to another.
-     */
+    /// @dev Moves delegated votes from one delegate to another.
     function _moveDelegateVotes(address from, address to, uint256 amount) private {
         ERC20DelegatesStorage storage $ = _getDelegatesStorage();
         if (from != to && amount > 0) {
@@ -147,20 +135,17 @@ abstract contract ERC20DelegatesUpgradeable is
         }
     }
 
-    /**
-     * @dev Must return the voting units held by an account.
-     */
+    /// @dev Must return the voting units held by an account.
     function _getVotingUnits(address account) internal view returns (uint256) {
         return balanceOf(account);
     }
 
-    /**
-     * @dev Move voting power when tokens are transferred.
-     *
-     * Emits a {IVotes-DelegateVotesChanged} event.
-     */
+    /// @dev Move voting power when tokens are transferred.
+    ///
+    /// Emits a {IDelegates-DelegateVotesChanged} event.
     function _update(address from, address to, uint256 value) internal virtual override {
         super._update(from, to, value);
+        // No check of supply cap here like in OZ implementation has MORPHO has a 1B total supply cap.
         _transferVotingUnits(from, to, value);
     }
 }

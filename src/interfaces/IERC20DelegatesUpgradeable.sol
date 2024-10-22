5e4467e2daba6984bd72271cb8ea8f96f4ac6ba9
MathisGD <mathis@morpho.xyz>
2024-10-22 16:15:34 +0200

    chore: fix merge issue

--- src/interfaces/IERC20DelegatesUpgradeable.sol
+++ src/interfaces/IERC20DelegatesUpgradeable.sol
@@ -11,19 +11,6 @@ interface IERC20DelegatesUpgradeable {
 
     function delegationNonce(address account) external view returns (uint256);
 
-<<<<<<< HEAD
-    // @dev Emitted when a token transfer or delegate change results in changes to a delegate's number of voting units.
-    event DelegateVotesChanged(address indexed delegate, uint256 previousVotes, uint256 newVotes);
-
-    // @dev Returns the current amount of votes that `delegator` has.
-    function delegatedVotingPower(address delegator) external view returns (uint256);
-
-    // @dev Returns the delegatee that `delegator` has chosen.
-    function delegatee(address delegator) external view returns (address);
-
-    // @dev Delegates votes from the sender to `delegatee`.
-=======
->>>>>>> style/docs-and-naming
     function delegate(address delegatee) external;
 
     function delegateBySig(address delegatee, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s) external;

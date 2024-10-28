a2733c8aaacbecd7954ed9a767e3d8247240d76b
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2024-10-28 16:49:52 +0100

    fix: order

--- src/interfaces/IOptimismMintableERC20.sol
+++ src/interfaces/IOptimismMintableERC20.sol
@@ -5,10 +5,10 @@ import {IERC165} from
     "../../lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/utils/introspection/IERC165.sol";
 
 /// @title IOptimismMintableERC20
+/// @custom:security-contact security@morpho.org
 /// @notice This interface is available on the OptimismMintableERC20 contract.
 ///         We declare it as a separate interface so that it can be used in
 ///         custom implementations of OptimismMintableERC20.
-/// @custom:security-contact security@morpho.org
 interface IOptimismMintableERC20 is IERC165 {
     function remoteToken() external view returns (address);
 

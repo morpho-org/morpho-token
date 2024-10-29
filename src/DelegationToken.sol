cd8ed9fedf540f1a37e2f5fc388c0b2c3d28f4c8
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2024-10-29 10:54:01 +0100

    fix: remove comment

--- src/DelegationToken.sol
+++ src/DelegationToken.sol
@@ -131,7 +131,6 @@ abstract contract DelegationToken is IDelegation, ERC20PermitUpgradeable, Ownabl
     }
 
     /// @dev Moves voting power when tokens are transferred.
-    /// @dev Emits a `IDelegation.DelegateVotesChanged` event.
     function _update(address from, address to, uint256 value) internal virtual override {
         super._update(from, to, value);
         _moveDelegateVotes(delegatee(from), delegatee(to), value);

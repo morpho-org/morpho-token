93c1e32b59a92e5f4ab05c3df7013c348ede3b9c
Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
2024-11-05 18:40:31 +0100

    fix: apply suggestions
    Co-authored-by: Quentin Garchery <QGarchery@users.noreply.github.com>
    Signed-off-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>

--- script/helpers/DeploymentImplementation.sol
+++ script/helpers/DeploymentImplementation.sol
@@ -1,15 +1,11 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (proxy/ERC1967/ERC1967Proxy.sol)
-
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
 
 import {UUPSUpgradeable} from "../../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 /// @title Deployment implementation for ERC1967Proxy
 /// @author Morpho Association
-/// @custom:contact security@morpho.org
-/// @dev Extension of UUPSUpgradeable.
-///
+/// @custom:security-contact security@morpho.org
 /// Contract meant to be the implementation of an ERC1967Proxy at deployment.
 contract DeploymentImplementation is UUPSUpgradeable {
     /// @inheritdoc UUPSUpgradeable

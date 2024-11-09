ceeea98fe092ebabdaf050f5545f8d2f989b6284
MathisGD <mathis@morpho.xyz>
2024-11-09 23:16:38 +0100

    test: fix import path

--- test/deployment/BaseDeploymentTest.sol
+++ test/deployment/BaseDeploymentTest.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {Test, console} from "../../lib/forge-std/src/Test.sol";
 
-import {DeployMorphoTokenBase} from "script/base/DeployMorphoTokenBase.sol";
+import {DeployMorphoTokenBase} from "script/DeployMorphoTokenBase.sol";
 
 import {IERC20} from
     "../../lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
--- test/deployment/EthereumDeploymentTest.sol
+++ test/deployment/EthereumDeploymentTest.sol
@@ -5,7 +5,7 @@ import {Test, console} from "../../lib/forge-std/src/Test.sol";
 
 import {MorphoTokenEthereum} from "../../src/MorphoTokenEthereum.sol";
 import {Wrapper} from "../../src/Wrapper.sol";
-import {DeployMorphoTokenEthereum} from "script/ethereum/DeployMorphoTokenEthereum.sol";
+import {DeployMorphoTokenEthereum} from "script/DeployMorphoTokenEthereum.sol";
 
 import {IERC20} from
     "../../lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";

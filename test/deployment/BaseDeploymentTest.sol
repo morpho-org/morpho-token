bebc0b274e43fe589c310d6855fe813ec49638f0
MathisGD <mathis@morpho.xyz>
2024-11-10 11:45:59 +0100

    test: improve deployment tests

--- test/deployment/BaseDeploymentTest.sol
+++ test/deployment/BaseDeploymentTest.sol
@@ -3,10 +3,8 @@ pragma solidity ^0.8.0;
 
 import {Test, console} from "../../lib/forge-std/src/Test.sol";
 
-import {DeployMorphoTokenBase} from "script/DeployMorphoTokenBase.sol";
-
-import {IERC20} from
-    "../../lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {DeployMorphoTokenBase} from "../../script/DeployMorphoTokenBase.sol";
+import {MorphoTokenOptimism} from "../../src/MorphoTokenOptimism.sol";
 
 contract EthereumDeploymentTest is DeployMorphoTokenBase, Test {
     address token;
@@ -17,6 +15,7 @@ contract EthereumDeploymentTest is DeployMorphoTokenBase, Test {
     }
 
     function testDeployment() public view {
-        assertEq(IERC20(token).totalSupply(), 0);
+        assertEq(MorphoTokenOptimism(token).totalSupply(), 0);
+        assertEq(MorphoTokenOptimism(token).owner(), MORPHO_DAO);
     }
 }
--- test/deployment/EthereumDeploymentTest.sol
+++ test/deployment/EthereumDeploymentTest.sol
@@ -5,10 +5,7 @@ import {Test, console} from "../../lib/forge-std/src/Test.sol";
 
 import {MorphoTokenEthereum} from "../../src/MorphoTokenEthereum.sol";
 import {Wrapper} from "../../src/Wrapper.sol";
-import {DeployMorphoTokenEthereum} from "script/DeployMorphoTokenEthereum.sol";
-
-import {IERC20} from
-    "../../lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {DeployMorphoTokenEthereum} from "../../script/DeployMorphoTokenEthereum.sol";
 
 contract EthereumDeploymentTest is DeployMorphoTokenEthereum, Test {
     address token;
@@ -21,7 +18,8 @@ contract EthereumDeploymentTest is DeployMorphoTokenEthereum, Test {
 
     function testDeployment() public view {
         assertEq(Wrapper(wrapper).NEW_MORPHO(), token);
-        assertEq(IERC20(token).totalSupply(), 1_000_000_000e18);
-        assertEq(IERC20(token).balanceOf(wrapper), 1_000_000_000e18);
+        assertEq(MorphoTokenEthereum(token).totalSupply(), 1_000_000_000e18);
+        assertEq(MorphoTokenEthereum(token).balanceOf(wrapper), 1_000_000_000e18);
+        assertEq(MorphoTokenEthereum(token).owner(), MORPHO_DAO);
     }
 }
